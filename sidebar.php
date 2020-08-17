4cb03cbbc7492382148ece3a5da83ba2094a3224
keisuke-tnk01 <ashitama18@gmail.com>
2020-08-18 08:48:34 +0900

    fix png images reading in sidebar & archive
    sidebarのキャンセルボタンとarchiveのページ送り画像読み込み修正

--- sidebar.php
+++ sidebar.php
@@ -1,41 +1,9 @@
 <sidebar class="l-sidebar">
     
             <div class="p-sidebar__wrapper">
-                <img src=".images/menu_cancel_btn.png" class="c-menu__cancel__btn" id="js-close__menu">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/menu_cancel_btn.png" class="c-menu__cancel__btn" id="js-close__menu">
                 <h2 class="c-sidebar__title">Menu</h2>
                 <div class="c-menu__list">
-                    <!--<div class="c-burger__menu">
-                        <h3>バーガー</h3>
-                        <ul>
-                            <li><a href="/">ハンバーガー</a></li>
-                            <li><a href="/">チーズバーガー</a></li>
-                            <li><a href="/">テリヤキバーガー</a></li>
-                            <li><a href="/">アボカドバーガー</a></li>
-                            <li><a href="/">フィッシュバーガー</a></li>
-                            <li><a href="/">ベーコンバーガー</a></li>
-                            <li><a href="/">チキンバーガー</a></li>
-                        </ul>
-                    </div>
-                    <div class="c-side__menu">
-                        <h3>サイド</h3>
-                        <ul>
-                            <li><a href="/">ポテト</a></li>
-                            <li><a href="/">サラダ</a></li>
-                            <li><a href="/">ナゲット</a></li>
-                            <li><a href="/">コーン</a></li>
-                        </ul>
-                    </div>
-                    <div class="c-drink__menu">
-                        <h3>ドリンク</h3>
-                        <ul>
-                            <li><a href="/">コーラ</a></li>
-                            <li><a href="/">ファンタ</a></li>
-                            <li><a href="/">オレンジ</a></li>
-                            <li><a href="/">アップル</a></li>
-                            <li><a href="/">紅茶（Ice/Hot）</a></li>
-                            <li><a href="/">コーヒー（Ice/Hot）</a></li>
-                        </ul>
-                    </div>-->
                     <?php wp_nav_menu(); ?> <!--サイドバーのメニューをgmenuから読み込み-->
                 </div>
             </div>
