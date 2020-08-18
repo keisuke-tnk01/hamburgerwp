b68958ecfbbe69807def57e4234486d8e85f3656
keisuke-tnk01 <ashitama18@gmail.com>
2020-08-18 13:14:32 +0900

    add pagenation to archive.php & search.php by WP Page Navi
    WP Page Naviを用いてarchive.phpとsearch.phpにページネーションを実装

--- search.php
+++ search.php
@@ -29,64 +29,13 @@
                 endif;
                 ?>
                 
-
+                <!--ページネーション-->
                 <section class="p-pagenation">
-                    <p class="c-page__view">page 1/10</p>
-                    <div class="c-page__previous">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/archive_page_previous.png" class="c-page__logo">
-                        <button type="button" class="c-page__word">前へ</button>
-                    </div>
-                    <div class="c-page__container">
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>1</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>2</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>3</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>4</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>5</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>6</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>7</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>8</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="c-page_number">
-                                <p>9</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="l-page_next">
-                        <button type="button" class="c-page__word">次へ</button>
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/archive_page_next.png" class="c-page__logo">
-                    </div>
+
+                    <p class="c-page__word__pre">前へ</p>
+                    <?php wp_pagenavi(); ?> <!--WP page naviによるページネーションのプラグイン-->
+                    <p class="c-page__word__nex">次へ</p>
+
                 </section>
             </div>
         </main>
