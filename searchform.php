79ce9bad21efbf87d9669363726a0930319f0d2f
keisuke-tnk01 <ashitama18@gmail.com>
2020-08-18 11:00:50 +0900

    add search form function & search.php
    検索機能を実装し、表示結果を示すsearch.phpを追加

--- searchform.php
+++ searchform.php
@@ -1,8 +1,6 @@
-<form role="search" method="get" action="<?php echo home_url('/'); ?>" id="searchform" class="p-search__form">
+<form role="search" method="get" action="<?php echo esc_url( home_url('/') ); ?>" id="searchform" class="p-search__form">
     <div>
-        <input type="text" class="c-search__text" placeholder="&#xf002;" name="s" id="s">
-            <label>
-                <input type="submit" class="c-search__submit" onclick="location.href='<?php the_permalink( $post ); ?>'" value="検索" id="searchsubmit">
-            </label>
+        <input class="c-search__text" type="text" placeholder="&#xf002;" name="s" id="s">
+        <input type="submit" class="c-search__submit" value="検索" id="searchsubmit">
     </div>
 </form>
